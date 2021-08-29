1b8729009407744099c6e91d9e9d072c6c7f6f5c
tthuyngann <89724852+tthuyngann@users.noreply.github.com>
2021-08-29 15:21:58 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "He luu banh duc!",
+  text2: "Em có điều này muốn hỏi anh nhớ phải trả lời thật lòng nhaaa.",
+  text3: "Anh đang nhớ em đúng khummmm ._.",
+  text4: "Nếu anh ko trả lời mà thoát ra em đánh nhau với anh đấy nhaaa =.=",
+  text5: "Em mơ à???",
+  text6: "Anh siu siu nhớ *.*",
+  text7: "lí do anh nhớ em ikkk :vvvv",
+  text8: "Gửi cho em <3",
+  text9: "Vì em babi xinh cute....em là số 1 siêu đỉnh >.<",
+  text10: "Em biết mà ^^ gitchee gitchee goo ",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "When i first saw u i looked for a signature, becau every masterpiece has one ^^",
+  text12: "yayyy <3",
 };
 
 $(document).ready(function () {
